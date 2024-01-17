bff57e8426bb76190113aa91935c747a7c51752e
janislaurisvilmanis <janis-lauris.vilmanis@edu.rtu.lv>
2024-01-17 15:26:09 +0200

    bibliotekas

---
+++ projekts.py
@@ -0,0 +1,7 @@
+import selenium
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.keys import Keys
+import time
+import openpyxl
+from openpyxl import Workbook, load_workbook
